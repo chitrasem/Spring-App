56469614d80c2ecf1a7a4f4603da0ea1ba7b9819
Sabaychitra84 <sabaychitra84@gmail.com>
2016-03-14 16:35:31 +0700

    updateed

--- kms-v2/src/main/java/com/chitra/kms/kh/controller/MainController.java
+++ kms-v2/src/main/java/com/chitra/kms/kh/controller/MainController.java
@@ -2,9 +2,19 @@ package com.chitra.kms.kh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("admin/dashboard")
 public class MainController {
+	
+	@RequestMapping(value="")
+	public ModelAndView dashboard(ModelAndView m){
+		m.setViewName("/dashboard/balnk");
+		
+		return m;
+	}
+	
+	
 
 }
