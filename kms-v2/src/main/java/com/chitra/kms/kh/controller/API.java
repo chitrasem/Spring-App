3390aff334397af6921cb8074670f2e083f69fdc
Sabaychitra84 <sabaychitra84@gmail.com>
2016-03-14 16:16:19 +0700

    Adding API Class

---
+++ kms-v2/src/main/java/com/chitra/kms/kh/controller/API.java
@@ -0,0 +1,11 @@
+package com.chitra.kms.kh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kms/api")
+public class API {
+	
+
+}
